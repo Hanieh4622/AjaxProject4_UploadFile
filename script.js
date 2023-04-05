8c4ec151307c20bf98711e0c2acdc864df25b0d8
Hanieh <Hanieh.a.a5015@gmail.com>
2023-04-05 21:04:29 +0430

    Add readystatechange

--- script.js
+++ script.js
@@ -18,9 +18,29 @@ $("#submit").click(function (e) {
             var percent=event.loaded / event.total;
             $('#uploadProgress').html(Math.round(percent*100) +'%');
         }
-        
-
     })
+
+    request.addEventListener('readystatechange',function(event){
+        if (this.readyState==4)
+        {
+            if(this.status==200){
+             
+                var response=JSON.parse(this.responseText);
+                //console.log(response)
+                $("#upload").html('');
+                html='';
+
+                for (var i = 0; i < response.length; i++) {
+                    html+='<div><a href="Files/'+response[i]+'" target="_blank">'+response[i]+'</a></div>';
+                }
+                $("#upload").append(html);
+            }
+            else{
+                alert("Error is"+this.status);
+            }
+        }
+    })
+
     request.upload.addEventListener('load',function(event){
         $('#uploadProgress').fadeOut();
         $('#uploadProgress').empty();
